9edf7e949677bb3f6a8cc847ec236c61eee59ce4
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2024-07-01 14:22:57 -0600

    fixing format

--- js/index.js
+++ js/index.js
@@ -1,29 +1,28 @@
-
-
-
  // Esto se llama fetch es para traer la información desde el link que se este llamando en este caso es el archivo json pero normalmente es de una api 
 function loadData() {
     return fetch('/data/BerryJerry.json')
       .then(response => response.json())
       .catch(error => console.error('Error loading the JSON:', error));
-  }
+}
 
 
 // Esta función es para renderizar la información que se trae desde el json que es donde van todas las demas funciones de render aqui puedes introducir las tuyas
-  function renderInfo(data) {
+function renderInfo(data) {
     renderHero(data);
     renderServices(data);
+    renderInfoFooter(data)
 }
 
 
 // Esta función es para renderizar el hero que es la parte de arriba de la pagina
 
-  function renderHero(data) {
+function renderHero(data) {
     document.getElementById('BerryJerry-hero').textContent = data.hero.title;
     document.getElementById('description-hero').textContent = data.hero.description;
     document.getElementById('background-hero').src = data.hero.image;
     document.getElementById('BerryJerry-icon-Id').src = data.iconImage;
 }
+
 
 // Esta función es para renderizar los servicios que se encuentran en el json 
 function renderServices(data) {
@@ -56,7 +55,10 @@ function renderServices(data) {
     });
 }
 
-
+function renderInfoFooter(data){
+  document.getElementById('footer-icon-id').src = data.iconImage;
+  document.getElementById('footer-desc-id').textContent = data.footerInfo.description;
+}
 
 
 // Esta función es para que se ejecute el contenido de la pagina cuando se cargue el contenido de la pagina
@@ -69,45 +71,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Esta función es para que se ejecute el contenido de la pagina cuando se cargue el contenido de la pagina que son simples animaciones 
 
-    const heroElements = document.querySelectorAll('.container-hero > *');
-    heroElements.forEach((element, index) => {
-    
-        element.style.animationDelay = `${index * 0.8}s`;
-        element.style.opacity = 1; 
-    });
+const heroElements = document.querySelectorAll('.container-hero > *');
+heroElements.forEach((element, index) => {
+
+  element.style.animationDelay = `${index * 0.8}s`;
+  element.style.opacity = 1;
+});
 
 
 
-    const fadeInSections = document.querySelectorAll('.fade-in-section');
+const fadeInSections = document.querySelectorAll('.fade-in-section');
 
-    const revealOnScroll = () => {
-        fadeInSections.forEach(section => {
-            const sectionTop = section.getBoundingClientRect().top;
-            const isVisible = sectionTop < window.innerHeight - 100; 
+const revealOnScroll = () => {
+  fadeInSections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isVisible = sectionTop < window.innerHeight - 100;
 
-            if (isVisible) {
-                section.style.opacity = 1;
-                section.style.transform = 'translateY(0)';
-            }
-        });
-    };
+    if (isVisible) {
+      section.style.opacity = 1;
+      section.style.transform = 'translateY(0)';
+    }
+  });
+};
 
-    window.addEventListener('scroll', revealOnScroll);
-    revealOnScroll();
+window.addEventListener('scroll', revealOnScroll);
+revealOnScroll();
 
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const servicesButton = document.getElementById('aboutus-hero');
-        servicesButton.addEventListener('click', () => {
-          document.getElementById('section-services').scrollIntoView({ behavior: 'smooth' });
-        });
-      });
+document.addEventListener('DOMContentLoaded', () => {
+  const servicesButton = document.getElementById('aboutus-hero');
+  servicesButton.addEventListener('click', () => {
+    document.getElementById('section-services').scrollIntoView({ behavior: 'smooth' });
+  });
+});
 
-      
-      document.addEventListener('DOMContentLoaded', () => {
-        const aboutUsLink = document.getElementById('about-us-link');
-        aboutUsLink.addEventListener('click', (e) => {
-          e.preventDefault(); 
-          document.getElementById('section-hero').scrollIntoView({ behavior: 'smooth' });
-        });
-      });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const aboutUsLink = document.getElementById('about-us-link');
+  aboutUsLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('section-hero').scrollIntoView({ behavior: 'smooth' });
+  });
+});
